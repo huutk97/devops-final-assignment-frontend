4365a8b7bb70efa20e8cc7e5595905c83133b1aa
trankhachuu <trankhachuu123@gmail.com>
2025-10-08 21:13:35 +0800

    code deploy cicd v0.3

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student Management APP HuuTK V0.3
+            Student Management APP HuuTK V0.4 code deploy cicd
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
